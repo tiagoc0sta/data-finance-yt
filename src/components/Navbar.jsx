6d5344b6b5eb6a1ad606e9e2c018d70af74f5f11
Tiago Costa <tiagodpv@gmail.com>
2022-11-29 16:29:25 -0500

    add condition for nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,14 @@
 //rafce
-import React from 'react'
+import React, {useState} from 'react'
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
 
 const Navbar = () => {
+  const [nav, setNav ] = useState(false)
+
+  const handleNav = () => {
+    setNav(!nav)
+  }
+
   return (
     <div className=' flex justify-between items-center h-24 max-w-[1240px] mx-auto text-white'>
       <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
@@ -14,15 +20,18 @@ const Navbar = () => {
         <li className='p-4'>Contact</li>
       </ul>
       <div>
-        <AiOutlineMenu size={20}/>
+        {!nav ? <AiOutlineClose size={20}/>: <AiOutlineMenu size={20}/> } {/* if nav is not true, display AiOutlineMenu else */}
+        
       </div>
-      <div className='fixed left-0 top-0 w-[60%] border-r border-r-gray-900'>
-        <ul className='pt-24 uppercase'>
-          <li className='p-4'>Home</li>
-          <li className='p-4'>Company</li>
-          <li className='p-4'>Resources</li>
-          <li className='p-4'>About</li>
-          <li className='p-4'>Contact</li>
+      <div className='fixed left-0 top-0 w-[60%] border-r border-r-gray-900 bg-[#000300]'>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
+        
+        <ul className='uppercase p-4'>
+          <li className='p-4 border-b border-gray-600'>Home</li>
+          <li className='p-4 border-b border-gray-600'>Company</li>
+          <li className='p-4 border-b border-gray-600'>Resources</li>
+          <li className='p-4 border-b border-gray-600'>About</li>
+          <li className='p-4 border-b border-gray-600'>Contact</li>
         </ul>
       </div>
       
