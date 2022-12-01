4c1d3aece1c8b64905b26d84fd253abdd3edde7b
Tiago Costa <tiagodpv@gmail.com>
2022-12-01 16:36:48 -0500

    editting cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -10,13 +10,13 @@ function Cards() {
         <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
           <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Single} alt="/" />
           <h2 className='text-2xl font-bold text-center py-8'>Single User</h2>
-          <p className='text-center text-4xl'>$149</p>
-          <div>
-            <p>500 GB Storage</p>
-            <p>1 Granted User</p>
-            <p>Send up to 2GB</p>
+          <p className='text-center text-4xl font-bold'>$149</p>
+          <div className='text-center font-medium'>
+            <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
+            <p className='py-2 border-b mx-8'>1 Granted User</p>
+            <p className='py-2 border-b mx-8'>Send up to 2GB</p>
           </div>
-          <button>Start Trial</button>
+          <button className='bg-[#00df9a] text-black rounded-md font-medium w-[200px] ml-4 my-6 px-6 py-3'>Start Trial</button>
         </div>       
       </div>
     </div>
