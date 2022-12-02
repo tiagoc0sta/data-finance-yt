f01ef54d3d65c7b837be2e8bbdce9b0b6ee1dbe5
Tiago Costa <tiagodpv@gmail.com>
2022-12-02 12:31:20 -0500

    add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  FaDribbbleSquare,
+  FaFacebookSquare,
+  FaGithubSquare,
+  FaInstagram,
+  FaTwitterSquare,
+} from 'react-icons/fa';
+
+const Footer = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
