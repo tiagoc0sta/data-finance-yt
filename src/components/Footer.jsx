0e5748465d9467be9f2197fbbe2d046f0fd8cee8
Tiago Costa <tiagodpv@gmail.com>
2022-12-02 16:21:52 -0500

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,19 @@ import {
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300'>
+      <div>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis quae debitis quibusdam dolore vel suscipit a hic architecto tenetur beatae mollitia facere voluptatem.</p>
+        <div className='flex justify-between md:w-[75%] my-6'>
+          <FaFacebookSquare size={30}/>
+          <FaInstagram size={30}/>
+          <FaTwitterSquare size={30}/>
+          <FaGithubSquare size={30}/>
+          <FaDribbbleSquare size={30}/>
+        </div>
+      </div>
+    </div>
   )
 }
 
